4b98ec76e906516acadc73c8277c5c49e2386cd4
Sharker3312 <fsharker0810@yahoo.com>
2024-04-12 17:35:35 -0400

    modificados:     terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,21 @@
 
 provider "aws" {
-  region = "us-east-2" # Reemplaza con tu región preferida
+  region = "us-east-2" 
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-00cda30cf72311684" # AMI de Amazon Linux 2 (Consulta la lista de AMI actualizada)
+  ami           = "ami-00cda30cf72311684" 
   instance_type = "t2.micro" 
-  key_name = module.ssh_key.ec2_key.key_name
-  security_groups = [module.security_group.security_group_id]             # Tipo de instancia en la capa gratuita
+  key_name = ec2_key
+  security_groups = [module.security_group.security_group_id]             
 
   tags = {
     Name = "Nodeapp"
   }
 }
 
-
-
 module "security_group" {
   source = "./module/security_group"
 }
-module "ssh_key" {
-  source = "./module/ssh_key"
 
-  
-}
 
