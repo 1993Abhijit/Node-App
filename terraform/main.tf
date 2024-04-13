ab674cd24ca659865d07f3883a404e768632f434
Sharker3312 <fsharker0810@yahoo.com>
2024-04-12 22:34:32 -0400

    modificados:     terraform/main.tf 	borrados:        terraform/module/security_group/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,8 +15,27 @@ resource "aws_instance" "ec2_instance" {
   }
 }
 
-module "security_group" {
-  source = "./module/security_group"
+resource "aws_security_group" "nodeapp" {
+  name        = "ec2_ecurity-group"
+  description = "Allow inbound traffic on port 8080 and SSH"
+  
+
+  
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
 
 
